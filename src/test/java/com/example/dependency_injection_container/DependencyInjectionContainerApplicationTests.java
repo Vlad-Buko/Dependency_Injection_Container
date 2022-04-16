b1f24383321fe5a10585ed02006eecd8b285aa55
Julietta <vald.viktor44@mail.ru>
2022-04-16 23:46:33 +0300

    Initial commit

---
+++ src/test/java/com/example/dependency_injection_container/DependencyInjectionContainerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dependency_injection_container;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DependencyInjectionContainerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
